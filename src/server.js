d5d72b37193239b5b3c44ffcd171f5d4dbc6b8e4
Krishna Patel <krp172005@gmail.com>
2026-04-28 23:06:54 -0400

    added health path

--- src/server.js
+++ src/server.js
@@ -16,6 +16,10 @@ const PORT = process.env.PORT || 8081;
 app.use(express.json());
 if(process.env.NODE_ENV !== 'test') app.use(morgan('tiny'));
 
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
 //read specification of yaml
 let specs;
 try{
@@ -55,8 +59,6 @@ app.use((error, req, res, next) => {
 if (process.env.NODE_ENV !== 'test') {
   app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 }
-app.get('/health', (req, res) => {
-  res.status(200).send('OK');
-});
+
 export default app;
 
